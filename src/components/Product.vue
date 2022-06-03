b2888acc86e11d45b651b6b73ea5624d561b6d33
wanlop poompanich <wanloppo@gmail.com>
2022-06-03 08:16:01 +0700

    edit data-table

---
+++ src/components/Product.vue
@@ -0,0 +1,233 @@
+<template>
+  <div class="q-pa-md">
+    <!-- <h3>Product</h3> -->
+    <form @submit.prevent="submitForm">
+      <label for="">DepartCode:</label>
+      <input type="text" ref="DepartCodeEl" maxlength="2" size="2" />
+      <label for=""> ProductCode:</label>
+      <input type="text" ref="ProductCodeEl" maxlength="7" size="7" />
+      <label for=""> ProductName:</label>
+      <input type="text" ref="ProductNameEl" maxlength="20" size="20" />&nbsp;
+      <button type="summit">Query</button>
+    </form>
+    <q-option-group
+      v-model="separator"
+      inline
+      class="q-mb-md"
+      :options="[
+        { label: 'Horizontal (default)', value: 'horizontal' },
+        { label: 'Vertical', value: 'vertical' },
+        { label: 'Cell', value: 'cell' },
+        { label: 'None', value: 'none' },
+      ]"
+    />
+    <q-table
+      color="primary"
+      card-class="bg-green-2 text-brown"
+      table-class="text-grey-8"
+      table-header-class="text-brown"
+      flat
+      title="Product"
+      :rows="rows"
+      :columns="columns"
+      :rows-per-page-options="[10, 15, 30]"
+      :separator="separator"
+      row-key="row_id"
+      :filter="filter"
+    >
+      <template v-slot:top-right>
+        <q-input
+          borderless
+          dense
+          debounce="300"
+          v-model="filter"
+          placeholder="Search"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
+  </div>
+</template>
+<script>
+import axios from "axios";
+import { ref } from "vue";
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: [],
+      Drive: "%",
+      FreePercent: 100,
+      dp_code: "01",
+      pr_code: "0001%",
+      pr_name: "%",
+      where: "",
+      url: "https://fastifyapi.azurewebsites.net/product",
+      wserver: "",
+      wdrive: "",
+      wfreepercent: "",
+      and_case: "",
+      sqlselect: "",
+      where_case: "",
+      firstName: "John",
+      lastName: "Doe",
+      offset: 0,
+      limit: 1000,
+      auth: "Authorization",
+      access_token:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0IjoxNjUxMjgxNDUwfQ.xaf3jCTryVA1l6E5ZJ8sONs-AaQPqM2kBGOELxaSdhs",
+      separator: ref("horizontal"),
+      filter: ref(""),
+      columns: [
+        {
+          name: "row_id",
+          required: true,
+          label: "No.",
+          align: "center",
+          field: "row_id",
+        },
+        {
+          name: "dp_code",
+          align: "center",
+          label: "DepartCode",
+          field: "dp_code",
+          sortable: true,
+        },
+        {
+          name: "dp_name",
+          align: "center",
+          label: "DepartName",
+          field: "dp_name",
+        },
+        {
+          name: "pr_code",
+          align: "center",
+          label: "ProductCode",
+          field: "pr_code",
+          sortable: true,
+        },
+        {
+          name: "pr_abb",
+          align: "left",
+          label: "ProductName",
+          field: "pr_abb",
+        },
+        {
+          name: "pr_engname",
+          align: "left",
+          label: "EngName",
+          field: "pr_engname",
+        },
+        {
+          name: "ba_nprice",
+          align: "right",
+          label: "RetailPrice",
+          field: "ba_nprice",
+          sortable: true,
+        },
+      ],
+      rows: [],
+    };
+  },
+  methods: {
+    submitForm() {
+      // alert('Query');
+      this.dp_code = this.$refs.DepartCodeEl.value;
+      this.dp_code = `${this.dp_code}%`;
+      this.pr_code = this.$refs.ProductCodeEl.value;
+      this.pr_code = `${this.pr_code}%`;
+      this.pr_name = this.$refs.ProductNameEl.value;
+      this.pr_name = `%${this.pr_name}%`;
+      this.where_case = this.getsql();
+      this.sqlselect = `${this.url}${this.where_case}`;
+
+      axios
+        // .get(this.sqlselect)
+        .create({ headers: { Authorization: this.access_token } })
+        .post(this.url, {
+          dp_code: this.dp_code,
+          pr_code: this.pr_code,
+          pr_name: this.pr_name,
+          offset: this.offset,
+          // limit: this.limit,
+        })
+        .then((response) => {
+          // JSON responses are automatically parsed.
+          // this.posts = response.data;
+          console.log("post ");
+          this.rows = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+    getsql() {
+      if (this.dp_code.length > 0) {
+        this.where = `dp_code=${this.dp_code}`;
+      } else {
+        this.where = `dp_code=%`;
+      }
+      // this.where = this.wserver;
+      this.and_case = "&";
+      if (this.pr_code.length > 0) {
+        this.where = `${this.where}${this.and_case}pr_code=${this.pr_code}%`;
+      } else {
+        this.where = `${this.where}${this.and_case}pr_code=%`;
+      }
+      if (this.pr_name.length > 0) {
+        this.where = `${this.where}${this.and_case}pr_name=%${this.pr_name}%`;
+      } else {
+        this.where = `${this.where}${this.and_case}pr_name=%`;
+      }
+      // console.log(this.where);
+      return this.where;
+    },
+    formatDate(dateString) {
+      const date = new Date(dateString);
+      // Then specify how you want your dates to be formatted
+      return new Intl.DateTimeFormat("th-TH", {
+        day: "2-digit",
+        // weekday: 'long',
+        month: "2-digit",
+        year: "numeric",
+        // hour: '2-digit',
+        // minute: '2-digit',
+        // second: '2-digit',
+        timeZone: "utc",
+      }).format(date);
+    },
+    formatNumber(number, digit) {
+      return new Intl.NumberFormat("en", {
+        maximumFractionDigits: digit,
+        minimumFractionDigits: digit,
+      }).format(number);
+    },
+  },
+  // Fetches posts when the component is created.
+  created() {
+    this.where_case = this.getsql();
+    this.sqlselect = `${this.url}${this.where_case}`;
+    axios
+      // .get(this.sqlselect)
+      .create({ headers: { Authorization: this.access_token } })
+      .post(this.url, {
+        dp_code: this.dp_code,
+        pr_code: this.pr_code,
+        pr_name: this.pr_name,
+        offset: this.offset,
+        // limit: this.limit,
+      })
+      .then((response) => {
+        this.rows = response.data;
+        // JSON responses are automatically parsed.
+        // this.posts = response.data;
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+};
+</script>
